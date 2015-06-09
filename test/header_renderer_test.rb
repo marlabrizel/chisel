e7aef8a60130360b233785a62d40a09db0543ee6
Marla Brizel <marlabrizel@gmail.com>
2015-06-08 19:52:43 -0600

    fix tests to accomodate refactored initialize

--- test/header_renderer_test.rb
+++ test/header_renderer_test.rb
@@ -5,43 +5,36 @@ require_relative '../lib/header_renderer'
 
 class HeaderRendererTest < Minitest::Test
 
-  def test_it_turns_a_single_hash_into_HTML_header
-    skip
-    heading = HeaderRenderer.new("#Heading 1")
-
-    assert_equal "<h1>Heading 1</h1>", heading.header_tag
-  end
-
   def test_it_counts_hashes_in_a_header
-    heading = ['#', 'H', 'e', 'a', 'd', 'i', 'n', 'g', '1']
+    heading = "#Heading 1"
     header = HeaderRenderer.new(heading)
 
     assert_equal 1, header.count_hashes
   end
 
   def test_it_counts_more_than_one_hash
-    heading = ['#', '#', 'H', 'e', 'a', 'd', 'i', 'n', 'g', '2']
+    heading = "##Heading 2"
     header = HeaderRenderer.new(heading)
 
     assert_equal 2, header.count_hashes
   end
 
   def test_it_strips_hashes_from_a_header
-    heading = ['#', '#', 'H', 'e', 'a', 'd', 'i', 'n', 'g']
+    heading = "##Heading"
     header = HeaderRenderer.new(heading)
 
     assert_equal ['H', 'e', 'a', 'd', 'i', 'n', 'g'], header.strip_hashes
   end
 
   def test_it_adds_HTML_tags_to_a_header
-    heading = ['#', 'H', 'e', 'a', 'd', 'i', 'n', 'g']
+    heading = "#Heading"
     header = HeaderRenderer.new(heading)
 
     assert_equal ['<h1>', 'H', 'e', 'a', 'd', 'i', 'n', 'g', '</h1>'], header.add_header_tags
   end
 
   def test_it_adds_proper_HTML_tag_for_number_of_hashes
-    heading = ['#', '#', 'H', 'e', 'a', 'd', 'i', 'n', 'g']
+    heading = "##Heading"
     header = HeaderRenderer.new(heading)
 
     assert_equal ['<h2>', 'H', 'e', 'a', 'd', 'i', 'n', 'g', '</h2>'], header.add_header_tags
--- test/paragraph_renderer_test.rb
+++ test/paragraph_renderer_test.rb
@@ -6,10 +6,10 @@ require_relative '../lib/paragraph_renderer'
 class ParagraphRendererTest < Minitest::Test
 
   def test_it_wraps_a_paragraph_in_tags
-    text = ['h', 'o', 'w', 'd', 'y']
+    text = "Howdy"
     paragraph = ParagraphRenderer.new(text)
 
-    assert_equal ['<p>', 'h', 'o', 'w', 'd', 'y', '</p>'], paragraph.add_paragraph_tags
+    assert_equal ['<p>', 'H', 'o', 'w', 'd', 'y', '</p>'], paragraph.add_paragraph_tags
   end
 
   #add edge case for newline

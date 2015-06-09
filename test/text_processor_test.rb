834c20507892be1dfaa9526ec22ae0b5ecf6d03b
Marla Brizel <marlabrizel@gmail.com>
2015-06-09 13:14:18 -0600

    initial commit

---
+++ test/text_processor_test.rb
@@ -0,0 +1,40 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'pry'
+require './lib/text_processor'
+
+class TextProcessorTest < Minitest::Test
+
+  def test_it_renders_a_md_header_in_HTML
+    input = "#Here is my header"
+    processor = TextProcessor.new(input)
+    output = processor.apply_rendering
+    expected = ["<h1>Here is my header</h1>"]
+
+    assert_equal expected, output
+  end
+
+  def test_it_renders_a_md_paragraph_in_HTML
+    skip
+  end
+
+  def test_it_renders_paragraphs_and_headers_correctly
+  #pass in a string that contains both a header and a
+  #paragraph and ensure both are processed correctyl
+    input = "#Header\n\nParagraph blah blah blah"
+    processor = TextProcessor.new(input)
+    output = processor.apply_rendering
+    expected = ["<h1>Header</h1>", "<p>Paragraph blah blah blah</p>"]
+
+    assert_equal expected, output
+  end
+  #
+  # def test_it_knows_a_paragraph_isnt_a_header
+  #   string = "Here is my paragraph."
+  #   divider = TextProcessor.new(string)
+  #
+  #   refute divider.is_header?
+  # end
+
+
+end

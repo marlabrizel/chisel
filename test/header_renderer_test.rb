5b52ab62a9304f3833aae09418d7f78b66b06cca
Marla Brizel <marlabrizel@gmail.com>
2015-06-09 13:14:01 -0600

    fix broken test

--- test/header_renderer_test.rb
+++ test/header_renderer_test.rb
@@ -5,39 +5,46 @@ require './lib/header_renderer'
 
 class HeaderRendererTest < Minitest::Test
 
-  def test_it_counts_hashes_in_a_header
-    heading = "#Heading 1"
-    header = HeaderRenderer.new(heading)
+  # def test_it_counts_hashes_in_a_header
+  #   heading = "#Heading 1"
+  #   header = HeaderRenderer.new(heading)
+  #
+  #   assert_equal 1, header.count_hashes
+  # end
+  #
+  # def test_it_counts_more_than_one_hash
+  #   heading = "##Heading 2"
+  #   header = HeaderRenderer.new(heading)
+  #
+  #   assert_equal 2, header.count_hashes
+  # end
+  #
+  # def test_it_strips_hashes_from_a_header
+  #   heading = "##Heading"
+  #   header = HeaderRenderer.new(heading)
+  #
+  #   assert_equal ['H', 'e', 'a', 'd', 'i', 'n', 'g'], header.strip_hashes
+  # end
+  #
+  # def test_it_adds_HTML_tags_to_a_header
+  #   heading = "#Heading"
+  #   header = HeaderRenderer.new(heading)
+  #
+  #   assert_equal ['<h1>', 'H', 'e', 'a', 'd', 'i', 'n', 'g', '</h1>'], header.add_header_tags
+  # end
+  #
+  # def test_it_adds_proper_HTML_tag_for_number_of_hashes
+  #   heading = "##Heading"
+  #   header = HeaderRenderer.new(heading)
+  #
+  #   assert_equal ['<h2>', 'H', 'e', 'a', 'd', 'i', 'n', 'g', '</h2>'], header.add_header_tags
+  # end
 
-    assert_equal 1, header.count_hashes
-  end
-
-  def test_it_counts_more_than_one_hash
-    heading = "##Heading 2"
-    header = HeaderRenderer.new(heading)
-
-    assert_equal 2, header.count_hashes
-  end
-
-  def test_it_strips_hashes_from_a_header
-    heading = "##Heading"
-    header = HeaderRenderer.new(heading)
-
-    assert_equal ['H', 'e', 'a', 'd', 'i', 'n', 'g'], header.strip_hashes
-  end
-
-  def test_it_adds_HTML_tags_to_a_header
+  def test_it_returns_an_HTML_marked_string
     heading = "#Heading"
     header = HeaderRenderer.new(heading)
 
-    assert_equal ['<h1>', 'H', 'e', 'a', 'd', 'i', 'n', 'g', '</h1>'], header.add_header_tags
-  end
-
-  def test_it_adds_proper_HTML_tag_for_number_of_hashes
-    heading = "##Heading"
-    header = HeaderRenderer.new(heading)
-
-    assert_equal ['<h2>', 'H', 'e', 'a', 'd', 'i', 'n', 'g', '</h2>'], header.add_header_tags
+    assert_equal "<h1>Heading</h1>", header.to_markup
   end
 
 end

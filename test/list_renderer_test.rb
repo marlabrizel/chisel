71b2cba28750b2a9852798fd26d1a1bd19eaf8e2
Marla Brizel <marlabrizel@gmail.com>
2015-06-09 21:21:39 -0600

    add tests for ordered lists

--- test/list_renderer_test.rb
+++ test/list_renderer_test.rb
@@ -2,7 +2,7 @@ require './test/test_helper'
 
 class ListRendererTest < Minitest::Test
 
-  def test_it_finds_one_list_item
+  def test_it_finds_one_unordered_list_item
     input = "* sushi\n"
     text = ListRenderer.new
     output = text.render_unordered(input)
@@ -11,7 +11,7 @@ class ListRendererTest < Minitest::Test
     assert_equal expected, output
   end
 
-  def test_it_finds_two_list_items
+  def test_it_finds_two_unordered_list_items
     input = "* sushi\n* sake\n"
     text = ListRenderer.new
     output = text.render_unordered(input)
@@ -20,7 +20,7 @@ class ListRendererTest < Minitest::Test
     assert_equal expected, output
   end
 
-  def test_it_finds_multiple_list_items
+  def test_it_finds_multiple_unordered_list_items
     input = "* sushi\n* sake\n* tempura\n"
     text = ListRenderer.new
     output = text.render_unordered(input)
@@ -33,10 +33,46 @@ class ListRendererTest < Minitest::Test
     input = "* sushi\n* sake\n* tempura\n"
     text = ListRenderer.new
     output = text.wrap_unordered(input)
-    # output.wrap_unordered(input)
     expected = "<ul><li>sushi</li><li>sake</li><li>tempura</li></ul>"
 
     assert_equal expected, output
   end
+
+  def test_it_finds_one_ordered_list_item
+    input = "1. sushi\n"
+    text = ListRenderer.new
+    output = text.render_ordered(input)
+    expected = "<li>sushi</li>"
+
+    assert_equal expected, output
+    end
+
+  def test_it_finds_two_ordered_list_items
+    input = "1. sushi\n2. sake\n"
+    text = ListRenderer.new
+    output = text.render_ordered(input)
+    expected = "<li>sushi</li><li>sake</li>"
+
+    assert_equal expected, output
+  end
+
+  def test_it_doesnt_care_what_numbers_are_used
+    input = "1. sushi\n1. sake\n"
+    text = ListRenderer.new
+    output = text.render_ordered(input)
+    expected = "<li>sushi</li><li>sake</li>"
+
+    assert_equal expected, output
+  end
+
+  def test_it_tags_the_start_and_end_of_an_ordered_list
+    input = "1. sushi\n2. sake\n3. tempura\n"
+    text = ListRenderer.new
+    output = text.wrap_ordered(input)
+    expected = "<ol><li>sushi</li><li>sake</li><li>tempura</li></ol>"
+
+    assert_equal expected, output
+  end
+
 
 end

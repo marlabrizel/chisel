a41fc82275f8195795e554cfe378666dcf8cb0f9
Marla Brizel <marlabrizel@gmail.com>
2015-06-09 19:28:04 -0600

    add strong renderer call

--- lib/chisel.rb
+++ lib/chisel.rb
@@ -1,10 +1,12 @@
-require_relative 'paragraph_renderer'
-require_relative 'header_renderer'
-require_relative 'assembler'
-require_relative 'text_divider'
+require_relative 'text_processor'
 
 # $ ruby ./lib/chisel.rb my_input.markdown my_output.html
 # Converted my_input.markdown (6 lines) to my_output.html (8 lines)
 markdown = File.read(ARGV[0])
 processor = TextProcessor.new(markdown)
+processor.apply_rendering
+markup = processor.rendered
+File.open(ARGV[1], "w") { |file| file.write(markup) }
+
 puts "Converted #{ARGV[0]} to #{ARGV[1]}"
+#write a test to ensure it takes a file and spits another out

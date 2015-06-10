017e48a15035bbe001274abfe8b7ab4a87571611
Marla Brizel <marlabrizel@gmail.com>
2015-06-09 19:27:22 -0600

    refactor tests to reflect method refactoring

--- test/strong_renderer_test.rb
+++ test/strong_renderer_test.rb
@@ -4,8 +4,8 @@ class StrongRendererTest < Minitest::Test
 
   def test_it_replaces_double_stars_with_strong_tags
     input = "**strong**"
-    text = StrongRenderer.new(input)
-    output = text.add_strong_tags
+    text = StrongRenderer.new
+    output = text.render(input)
     expected = "<strong>strong</strong>"
 
     assert_equal expected, output
@@ -13,8 +13,8 @@ class StrongRendererTest < Minitest::Test
 
   def test_it_replaces_double_stars_within_string_with_strong_tags
     input = "This text is **strong**"
-    text = StrongRenderer.new(input)
-    output = text.add_strong_tags
+    text = StrongRenderer.new
+    output = text.render(input)
     expected = "This text is <strong>strong</strong>"
 
     assert_equal expected, output
@@ -22,8 +22,8 @@ class StrongRendererTest < Minitest::Test
 
   def test_it_replaces_single_stars_with_em_tags
     input = "*italics*"
-    text = StrongRenderer.new(input)
-    output = text.add_emphasis_tags
+    text = StrongRenderer.new
+    output = text.render(input)
     expected = "<em>italics</em>"
 
     assert_equal expected, output
@@ -31,8 +31,8 @@ class StrongRendererTest < Minitest::Test
 
   def test_it_finds_multiple_instances_of_emphases
     input = "*italics* plus more *italics*"
-    text = StrongRenderer.new(input)
-    output = text.add_emphasis_tags
+    text = StrongRenderer.new
+    output = text.render(input)
     expected = "<em>italics</em> plus more <em>italics</em>"
 
     assert_equal expected, output
@@ -40,8 +40,8 @@ class StrongRendererTest < Minitest::Test
 
   def test_it_finds_both_strong_and_italics_and_replaces_them
     input = "*italics* plus some **bold things**"
-    text = StrongRenderer.new(input)
-    output = text.add_emphasis_tags
+    text = StrongRenderer.new
+    output = text.render(input)
     expected = "<em>italics</em> plus some <strong>bold things</strong>"
 
     assert_equal expected, output
@@ -49,8 +49,8 @@ class StrongRendererTest < Minitest::Test
 
   def test_it_replaces_for_nested_strongs_and_italics
     input = "This *test is **totally** going* to break"
-    text = StrongRenderer.new(input)
-    output = text.add_emphasis_tags
+    text = StrongRenderer.new
+    output = text.render(input)
     expected = "This <em>test is <strong>totally</strong> going</em> to break"
 
     assert_equal expected, output
@@ -58,8 +58,8 @@ class StrongRendererTest < Minitest::Test
 
   def test_it_does_not_render_a_lonely_asterisk
     input = "*This should be left alone"
-    text = StrongRenderer.new(input)
-    output = text.add_emphasis_tags
+    text = StrongRenderer.new
+    output = text.render(input)
     expected = "*This should be left alone"
 
     assert_equal expected, output

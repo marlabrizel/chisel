b3f4bce689074089f2a6118371a458ed7d9392b8
Marla Brizel <marlabrizel@gmail.com>
2015-06-10 17:53:22 -0600

    update tests

--- test/link_renderer_test.rb
+++ test/link_renderer_test.rb
@@ -3,10 +3,19 @@ require './test/test_helper'
 class LinkRendererTest < Minitest::Test
 
   def test_it_renders_a_basic_link
-    input = '[link](http://hi.com/)'
+    input = "[link](http://hi.com/)"
     text = LinkRenderer.new(input)
     output = text.process_link
     expected = "<a href='http://hi.com/'>link</a>"
+
+    assert_equal expected, output
+  end
+
+  def test_it_finds_a_link_in_text
+    input = "Chipotle [yum](http://www.chipotle.com/)"
+    text = LinkRenderer.new(input)
+    output = text.process_link
+    expected = "Chipotle <a href='http://www.chipotle.com/'>yum</a>"
 
     assert_equal expected, output
   end

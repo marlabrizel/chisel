3765d02c99c1e47e50a2ab0158ed0c326bdb2a9d
Marla Brizel <marlabrizel@gmail.com>
2015-06-09 22:24:51 -0600

    all of the refactoring

--- lib/list_renderer.rb
+++ lib/list_renderer.rb
@@ -1,29 +1,35 @@
 class ListRenderer
 
-  def render_unordered(input)
-    incoming = input
-    while incoming.include?("* ")
-      incoming.sub!("* ", "<li>").sub!("\n", "</li>")
+attr_reader :input
+
+  NUMBER_PREFIXES = (0..1000).map { |n| "#{n}. " }
+
+  def initialize(input)
+    @input = input
+  end
+
+  def process_unordered
+    while @input.include?("* ")
+      @input.sub!("* ", "<li>").sub!("\n", "</li>")
     end
-    incoming
+    @input
   end
 
-  def wrap_unordered(input)
-    render_unordered(input).insert(0, "<ul>").insert(-1, "</ul>")
+  def wrap_unordered
+    process_unordered.insert(0, "<ul>").insert(-1, "</ul>")
   end
 
-  def render_ordered(input)
-    incoming = input
-    (0..1000).each do |number|
-      while incoming.include?("#{number}. ")
-        incoming.sub!("#{number}. ", "<li>").sub!("\n", "</li>")
+  def process_ordered
+    NUMBER_PREFIXES.each do |number|
+      while @input.include?("#{number}")
+        @input.sub!("#{number}", "<li>").sub!("\n", "</li>")
       end
     end
-    incoming
+    @input
   end
 
-  def wrap_ordered(input)
-    render_ordered(input).insert(0, "<ol>").insert(-1, "</ol>")
+  def wrap_ordered
+    process_ordered.insert(0, "<ol>").insert(-1, "</ol>")
   end
 
 end
--- test/list_renderer_test.rb
+++ test/list_renderer_test.rb
@@ -4,8 +4,8 @@ class ListRendererTest < Minitest::Test
 
   def test_it_finds_one_unordered_list_item
     input = "* sushi\n"
-    text = ListRenderer.new
-    output = text.render_unordered(input)
+    text = ListRenderer.new(input)
+    output = text.process_unordered
     expected = "<li>sushi</li>"
 
     assert_equal expected, output
@@ -13,8 +13,8 @@ class ListRendererTest < Minitest::Test
 
   def test_it_finds_two_unordered_list_items
     input = "* sushi\n* sake\n"
-    text = ListRenderer.new
-    output = text.render_unordered(input)
+    text = ListRenderer.new(input)
+    output = text.process_unordered
     expected = "<li>sushi</li><li>sake</li>"
 
     assert_equal expected, output
@@ -22,8 +22,8 @@ class ListRendererTest < Minitest::Test
 
   def test_it_finds_multiple_unordered_list_items
     input = "* sushi\n* sake\n* tempura\n"
-    text = ListRenderer.new
-    output = text.render_unordered(input)
+    text = ListRenderer.new(input)
+    output = text.process_unordered
     expected = "<li>sushi</li><li>sake</li><li>tempura</li>"
 
     assert_equal expected, output
@@ -31,8 +31,8 @@ class ListRendererTest < Minitest::Test
 
   def test_it_tags_the_start_and_end_of_an_unordered_list
     input = "* sushi\n* sake\n* tempura\n"
-    text = ListRenderer.new
-    output = text.wrap_unordered(input)
+    text = ListRenderer.new(input)
+    output = text.wrap_unordered
     expected = "<ul><li>sushi</li><li>sake</li><li>tempura</li></ul>"
 
     assert_equal expected, output
@@ -40,8 +40,8 @@ class ListRendererTest < Minitest::Test
 
   def test_it_finds_one_ordered_list_item
     input = "1. sushi\n"
-    text = ListRenderer.new
-    output = text.render_ordered(input)
+    text = ListRenderer.new(input)
+    output = text.process_ordered
     expected = "<li>sushi</li>"
 
     assert_equal expected, output
@@ -49,8 +49,8 @@ class ListRendererTest < Minitest::Test
 
   def test_it_finds_two_ordered_list_items
     input = "1. sushi\n2. sake\n"
-    text = ListRenderer.new
-    output = text.render_ordered(input)
+    text = ListRenderer.new(input)
+    output = text.process_ordered
     expected = "<li>sushi</li><li>sake</li>"
 
     assert_equal expected, output
@@ -58,8 +58,8 @@ class ListRendererTest < Minitest::Test
 
   def test_it_doesnt_care_what_numbers_are_used
     input = "1. sushi\n1. sake\n"
-    text = ListRenderer.new
-    output = text.render_ordered(input)
+    text = ListRenderer.new(input)
+    output = text.process_ordered
     expected = "<li>sushi</li><li>sake</li>"
 
     assert_equal expected, output
@@ -67,8 +67,8 @@ class ListRendererTest < Minitest::Test
 
   def test_it_tags_the_start_and_end_of_an_ordered_list
     input = "1. sushi\n2. sake\n3. tempura\n"
-    text = ListRenderer.new
-    output = text.wrap_ordered(input)
+    text = ListRenderer.new(input)
+    output = text.wrap_ordered
     expected = "<ol><li>sushi</li><li>sake</li><li>tempura</li></ol>"
 
     assert_equal expected, output

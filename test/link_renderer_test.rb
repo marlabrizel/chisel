d04f723282fed9ee67f47ba16343ae5ba1c29d54
Marla Brizel <marlabrizel@gmail.com>
2015-06-10 13:25:20 -0600

    initial commit

---
+++ test/link_renderer_test.rb
@@ -0,0 +1,14 @@
+require './test/test_helper'
+
+class LinkRendererTest < Minitest::Test
+
+  def test_it_renders_a_basic_link
+    input = '[link](http://hi.com/)'
+    text = LinkRenderer.new(input)
+    output = text.process_link
+    expected = "<a href='http://hi.com/'>link</a>"
+
+    assert_equal expected, output
+  end
+
+end

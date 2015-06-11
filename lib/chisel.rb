454b40b32a588b92774b810a6bb751a25c977ec3
Marla Brizel <marlabrizel@gmail.com>
2015-06-10 21:18:27 -0600

    add file line count

--- lib/chisel.rb
+++ lib/chisel.rb
@@ -1,6 +1,5 @@
 require_relative 'text_processor'
 
-# $ ruby ./lib/chisel.rb my_input.markdown my_output.html
 # Converted my_input.markdown (6 lines) to my_output.html (8 lines)
 markdown = File.read(ARGV[0])
 processor = TextProcessor.new(markdown)
@@ -8,5 +7,5 @@ processor.apply_rendering
 markup = processor.rendered
 File.open(ARGV[1], "w") { |file| file.write(markup) }
 
-puts "Converted #{ARGV[0]} to #{ARGV[1]}"
+puts "Converted #{ARGV[0]} (#{processor.count_input_lines} lines) to #{ARGV[1]}"
 #write a test to ensure it takes a file and spits another out

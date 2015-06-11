290efcad830923dfd86277667ed000f561c4ca94
Marla Brizel <marlabrizel@gmail.com>
2015-06-10 20:19:27 -0600

    all of the refactoring

--- lib/header_renderer.rb
+++ lib/header_renderer.rb
@@ -1,17 +1,15 @@
 class HeaderRenderer
 
-attr_accessor :header
-
   def initialize(header)
     @header = header.split("")
     add_header_tags
-    # this breaks all of my tests even though
-    # it works, so what's the right level of testing
   end
 
   def to_markup
     @header.join("")
   end
+
+  private
 
   def count_hashes
     count = 0
@@ -21,36 +19,17 @@ attr_accessor :header
       end
     end
     count
-    #count hashes
   end
 
-  def strip_hashes
+  def strip_hashes!
     @header.delete_if {|char| char == '#'}
-    #remove hashes
   end
 
   def add_header_tags
-    if count_hashes == 1
-      self.strip_hashes.unshift('<h1>')
-      self.strip_hashes.push('</h1>')
-    elsif count_hashes == 2
-      self.strip_hashes.unshift('<h2>')
-      self.strip_hashes.push('</h2>')
-    elsif count_hashes == 3
-      self.strip_hashes.unshift('<h3>')
-      self.strip_hashes.push('</h3>')
-    elsif count_hashes == 4
-      self.strip_hashes.unshift('<h4>')
-      self.strip_hashes.push('</h4>')
-    elsif count_hashes == 5
-      self.strip_hashes.unshift('<h5>')
-      self.strip_hashes.push('</h5>')
+      count = count_hashes
+      strip_hashes!
+      @header.unshift("<h#{count}>")
+      @header << ("</h#{count}>")
     end
-    #string concatenation
-    #count hashes, know how many to delete from front
-    #turn header tags into variables and incorporate count
-    # "<h#{count}>" + text + "</h#{count}>"
-    #we want to replace a hash at the beginning of an array with <h1> and </h1> at either end of the array
-  end
 
 end
--- test/header_renderer_test.rb
+++ test/header_renderer_test.rb
@@ -5,46 +5,18 @@ require './lib/header_renderer'
 
 class HeaderRendererTest < Minitest::Test
 
-  # def test_it_counts_hashes_in_a_header
-  #   heading = "#Heading 1"
-  #   header = HeaderRenderer.new(heading)
-  #
-  #   assert_equal 1, header.count_hashes
-  # end
-  #
-  # def test_it_counts_more_than_one_hash
-  #   heading = "##Heading 2"
-  #   header = HeaderRenderer.new(heading)
-  #
-  #   assert_equal 2, header.count_hashes
-  # end
-  #
-  # def test_it_strips_hashes_from_a_header
-  #   heading = "##Heading"
-  #   header = HeaderRenderer.new(heading)
-  #
-  #   assert_equal ['H', 'e', 'a', 'd', 'i', 'n', 'g'], header.strip_hashes
-  # end
-  #
-  # def test_it_adds_HTML_tags_to_a_header
-  #   heading = "#Heading"
-  #   header = HeaderRenderer.new(heading)
-  #
-  #   assert_equal ['<h1>', 'H', 'e', 'a', 'd', 'i', 'n', 'g', '</h1>'], header.add_header_tags
-  # end
-  #
-  # def test_it_adds_proper_HTML_tag_for_number_of_hashes
-  #   heading = "##Heading"
-  #   header = HeaderRenderer.new(heading)
-  #
-  #   assert_equal ['<h2>', 'H', 'e', 'a', 'd', 'i', 'n', 'g', '</h2>'], header.add_header_tags
-  # end
-
   def test_it_returns_an_HTML_marked_string
     heading = "#Heading"
     header = HeaderRenderer.new(heading)
 
     assert_equal "<h1>Heading</h1>", header.to_markup
+  end
+
+  def test_it_returns_another_HTML_marked_string
+    heading = "##Heading 2"
+    header = HeaderRenderer.new(heading)
+
+    assert_equal "<h2>Heading 2</h2>", header.to_markup
   end
 
 end
